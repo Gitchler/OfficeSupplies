988d0d97485d5805c54d1b25a0fc1d1924173ae4
Luke Haenisch <lukehaenisch@Lukes-MacBook-Air.local>
2024-02-01 10:11:56 -0800

    Created clock java

---
+++ Clock.java
@@ -0,0 +1,47 @@
+/**
+ * Author - TheG0dOfGit + Gitchler
+ * Version - 1/31
+ */
+
+public class Clock
+{
+    /**
+     * declare instance variables.
+     */
+    private static int hours;
+    private static int mins;
+    private static int secs;
+    private static double batt;
+
+     /**
+      * Method getTime returns the time in hours, mins, and secs.
+      * @return Time the time in hours, mins, and secs.
+      */
+    public static int getTime()
+    {
+    }
+
+    /**
+     * Method setTime sets the by hours, mins, and secs. 
+     * @param Hours,Mins,Secs the hours, mins, and secs to be set. 
+     */
+    public static void setTime()
+    {
+    }
+
+    /**
+     * Method getBatt returns the percentage of the battery left in the clock
+     * @return batt the percentage of battery left in the clock.
+     */
+    public static double getBatt()
+    {
+        return batt;
+    }
+    /**
+     * Method replaceBatt replenishes the Battery in the clock
+     */
+    public static void replaceBatt()
+    {
+    }
+    
+}
