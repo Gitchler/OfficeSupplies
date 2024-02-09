13cc7d7c9865a7229a2b5cad10cbe867ebba29e6
Luke Haenisch <lhaenisch24@students.polytechnic.org>
2024-02-08 19:03:42 -0800

    added comments to all methods

--- Portfolio.java
+++ Portfolio.java
@@ -1,5 +1,5 @@
 /**
- * File for the Bloomberg Terminal class to be used in the Office Supplies project.
+ * The Portfolio class allows the user to interact with the stocks in the StockMarket class ina  variety of manners. 
  * @author TheG0dOfGit and Gitchler
  * @version 2/3/2024
  */
@@ -10,9 +10,8 @@ public class Portfolio
     /**
      * declaring instance variables.
      */
-    private ArrayList<Stock> Portfolio;
+    private ArrayList<Stock> portfolio;
     private StockMarket market = new StockMarket();
-    private ArrayList<Stock>Market;
     private double liquidity = 100000;
     
     
@@ -21,24 +20,32 @@ public class Portfolio
      */
     public Portfolio()
     {
-        Portfolio = new ArrayList<Stock>();
+        portfolio = new ArrayList<Stock>();
     }
-        
-    public String fullWatchlist(StockMarket Market)
+    
+    /**
+     * fullWatchlist prints out the full list of stocks in the StockMarket 
+     * @param the StockMarket (or stock exchange such as NYSE or NASDAQ) to be accessed. 
+     */
+    public void fullWatchlist(StockMarket market)
     {
         ArrayList<Stock> thisMarket = new ArrayList<Stock>();
-        thisMarket = Market.getStocks();
+        thisMarket = market.getStocks();
         for (Stock stock : thisMarket)
         {
             System.out.println(stock.getTicker());
         }
-        return null;
     }
     
-    public String sectorScreener(String thisSector, StockMarket Market)
+    /**
+     * sectorScreener prints out a screened list of stocks that belong to a specified sector. This function would be useful for an investor who is bullish on a particular sector. 
+     * @param thisSector the sector to screen for
+     * @param market the StockMarket to access
+     */
+    public String sectorScreener(String thisSector, StockMarket market)
     {
         ArrayList<Stock> thisMarket = new ArrayList<Stock>();
-        thisMarket = Market.getStocks();
+        thisMarket = market.getStocks();
         for (Stock stock : thisMarket)
         {
             if (stock.getSector().equals(thisSector))
@@ -49,10 +56,15 @@ public class Portfolio
         return null;
     }
     
-    public String valueScreener(double maxPE, StockMarket Market)
+    /**
+     * valueScreener prints out a screened list of stocks that have a price/earnings ratio below a specified maximum. The price/earnings ratio is the most fundamental measure of how over/under valued a stock is. This function would be useful for a value investor. 
+     * @param maxPE the maximum price/earnings ratio to screen for
+     * @param market the StockMarket to access
+     */
+    public void valueScreener(double maxPE, StockMarket market)
     {
         ArrayList<Stock> thisMarket = new ArrayList<Stock>();
-        thisMarket = Market.getStocks();
+        thisMarket = market.getStocks();
         for (Stock stock : thisMarket)
         {
             if (stock.getPE() <= maxPE)
@@ -60,12 +72,17 @@ public class Portfolio
                  System.out.println(stock.getTicker());
             }
         }
-        return null;
     }
-    public String divScreener(double minDivYield, StockMarket Market)
+    
+    /**
+     * divScreener prints out a screened list of stocks that have a dividend yield above a specified minimum. This function would be useful for a value investor or cash-flow centric investor. 
+     * @param thisSector the sector to screen for
+     * @param market the StockMarket to access
+     */
+    public void divScreener(double minDivYield, StockMarket market)
     {
         ArrayList<Stock> thisMarket = new ArrayList<Stock>();
-        thisMarket = Market.getStocks();
+        thisMarket = market.getStocks();
         for (Stock stock : thisMarket)
         {
             if (stock.getDivYield() >= minDivYield)
@@ -73,17 +90,28 @@ public class Portfolio
                 System.out.println(stock.getTicker());
             }
         }
-        return null;
     }
+    
+    /**
+     * getLiquidity returns the liquidity (remaining cash balance) of the portfolio. 
+     * @return liquidity
+     */
     public double getLiquidity()
     {
         return liquidity;
     }
-    public void buy(String tickerToBuy, double quantity, StockMarket Market)
+    
+    /**
+     * buy purchases, or adds, a specified stock listed in the specified StockMarket at a specified quantity to the portfolio.
+     * @param tickerToBuy the ticker of the stock to be "bought"
+     * @param quantity the number of stocks to be "bought"
+     * @param market the StockMarket in which the ticker of the stock is searched for
+     */
+    public void buy(String tickerToBuy, double quantity, StockMarket market)
     {
         Stock toBeBought = null;
         ArrayList<Stock> thisMarket = new ArrayList<Stock>();
-        thisMarket = Market.getStocks();
+        thisMarket = market.getStocks();
         for (Stock stock : thisMarket)
         {
             if (stock.getTicker().equals(tickerToBuy))
@@ -92,14 +120,18 @@ public class Portfolio
             }
         }
         toBeBought.setQuantity(quantity);
-        Portfolio.add(toBeBought);
+        portfolio.add(toBeBought);
         liquidity = liquidity - (toBeBought.getPrice() * quantity);
         System.out.println("New position " + toBeBought.getTicker() + " | Position size: " + " (" + (toBeBought.getPrice() * quantity) + ")");
         System.out.println("new liquidity = " + liquidity);
     }
+    
+    /**
+     * viewPortfolio prints out each stock and its quantity within the portfolio. 
+     */
     public void viewPortfolio()
     {
-        for (Stock stock : Portfolio)
+        for (Stock stock : portfolio)
         {
             System.out.println(stock.getTicker() + " " + "(" + stock.getPrice() + ")" + " " + " (" + stock.getQuantity() + ")");
         }
