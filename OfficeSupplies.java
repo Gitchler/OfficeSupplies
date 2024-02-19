94d9fe2c98884079f5ccd154c61fe960a33a6899
Luke Haenisch <lhaenisch24@students.polytechnic.org>
2024-02-19 11:55:39 -0800

    Fixed everything

--- OfficeSupplies.java
+++ OfficeSupplies.java
@@ -83,5 +83,84 @@ public class OfficeSupplies
         Clock d = new Clock("6", "45");
         System.out.println("Getting the time...");
         System.out.println(d.getTime());
+        
+        
+        //PORTFOLIO TESTER
+        System.out.println("-----------------");
+        System.out.println("Now testing the Portfolio class");
+        Portfolio TheTradingTavern = new Portfolio();
+        System.out.println("Initalizing a portfolio...");
+        System.out.println("Portfolio initialized: TheTradingTavern");
+        System.out.println("Liquidity available: " + TheTradingTavern.getLiquidity());
+        
+        StockMarket BigStock = new StockMarket();
+        
+        System.out.println("----------------");
+        
+        System.out.println("Initializing the market");
+        
+        System.out.println("----------------");
+        
+        Stock AAPL = new Stock("AAPL", "Information Technology", 185.85, 0.52, 6.43);
+        BigStock.marketMaking(AAPL);
+        Stock META = new Stock("META", "Information Technology", 474.99, 0.42, 14.86);
+        BigStock.marketMaking(META);
+        Stock GOOG = new Stock("GOOG", "Information Technology", 143.54, 0, 5.80);
+        BigStock.marketMaking(GOOG);
+        Stock NVDA = new Stock("NVDA", "Information Technology", 639.74, 0.02, 7.61);
+        BigStock.marketMaking(NVDA);
+        Stock AMZN = new Stock("AMZN", "Information Technology", 185.04, 0, 4.30);
+        BigStock.marketMaking(AMZN);
+        
+        Stock NKE = new Stock("NKE", "Consumer Discretionary", 100.88, 1.47, 3.43);
+        BigStock.marketMaking(NKE);
+        Stock SBUX = new Stock("SBUX", "Consumer Discretionary", 92.69, 2.45, 3.75);
+        BigStock.marketMaking(SBUX);
+        Stock MCD = new Stock("MCD", "Consumer Discretionary", 296.07, 2.25, 11.35);
+        BigStock.marketMaking(MCD);
+        
+        Stock LMT = new Stock("LMT", "Industrials", 428.14, 2.96, 27.56);
+        BigStock.marketMaking(LMT);
+        Stock BA = new Stock("BA", "Industrials", 209.06, 0, -3.66);
+        BigStock.marketMaking(BA);
+        Stock RTX = new Stock("RTX", "Industrials", 92.94, 2.57, 41.32);
+        BigStock.marketMaking(RTX);
+
+        Stock GS = new Stock("GS", "Financials", 381.80, 2.84, 16.96);
+        BigStock.marketMaking(GS);
+        Stock JPM = new Stock("JPM", "Financials", 173.30, 2.40, 10.77);
+        BigStock.marketMaking(JPM);
+        
+        System.out.println("Displaying the market");
+        
+        System.out.println("----------------");
+        
+        BigStock.viewMarket();
+        
+        System.out.println("Screening the market for a maximum P/E ratio of 20.00 (the price to earnings ratio is the most common indicator of a stock being over/under value)");
+        TheTradingTavern.valueScreener(20.00, BigStock);
+        System.out.println("----------------");
+        
+        System.out.println("Screening the market for a minimum dividend yield of 1.00");
+        TheTradingTavern.divScreener(1.00, BigStock);
+        System.out.println("----------------");
+        
+        System.out.println("Screening the market for the Information Technology sector");
+        TheTradingTavern.sectorScreener("Information Technology", BigStock);
+        System.out.println("----------------");
+        
+        System.out.println("Buying 3 shares of AAPL and 2 shares of LMT");
+        TheTradingTavern.buy("AAPL", 3, BigStock);
+        TheTradingTavern.buy("LMT", 2, BigStock);
+        System.out.println("----------------");
+        
+        System.out.println("Printing out remaining liquidity");
+        System.out.println(TheTradingTavern.getLiquidity());
+        System.out.println("----------------");
+        
+        System.out.println("Viewing portfolio");
+        TheTradingTavern.viewPortfolio();
+        
+
     }
 }
